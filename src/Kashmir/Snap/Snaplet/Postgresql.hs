88d59b9befda744a432bdc6b5b6d3b6538feec47
Kris Jenkins <krisajenkins@gmail.com>
2015-08-10 14:08:40 +0100

    Fixing some problems with the Snaplet compilation.
    I still have to get used to working on two projects in parallel!

--- src/Kashmir/Snap/Snaplet/Postgresql.hs
+++ src/Kashmir/Snap/Snaplet/Postgresql.hs
@@ -4,7 +4,7 @@ import           Control.Lens
 import           Control.Monad.IO.Class
 import           Control.Monad.Logger
 import           Control.Monad.State.Class
-import           Data.ByteString
+import qualified Data.ByteString.Char8       as C
 import           Database.Esqueleto          hiding (get)
 import           Database.Persist            ()
 import           Database.Persist.Postgresql (createPostgresqlPool)
@@ -15,8 +15,8 @@ initDb :: DbConfig -> SnapletInit a ConnectionPool
 initDb config =
   makeSnaplet "connection-pool" "A simple Postgresql connection pool" Nothing .
   liftIO . runStdoutLoggingT $
-  createPostgresqlPool (view config connectionString)
-                       (view config poolSize)
+  createPostgresqlPool (C.pack (view connectionString config))
+                       (view poolSize config)
 
 sqlHandler :: SqlPersistM b -> Handler a ConnectionPool b
 sqlHandler query =
