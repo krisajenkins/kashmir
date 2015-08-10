0e37fb1779b4aa1f4db6ff2808774f59483afd16
Kris Jenkins <krisajenkins@gmail.com>
2015-08-10 12:45:40 +0100

    Adding a simple DB Snaplet.

---
+++ src/Kashmir/Snap/Snaplet/Postgresql.hs
@@ -0,0 +1,25 @@
+module Kashmir.Snap.Snaplet.Postgresql where
+
+import           Control.Lens
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Control.Monad.State.Class
+import           Data.ByteString
+import           Database.Esqueleto          hiding (get)
+import           Database.Persist            ()
+import           Database.Persist.Postgresql (createPostgresqlPool)
+import           Kashmir.Database.Postgresql
+import           Snap
+
+initDb :: DbConfig -> SnapletInit a ConnectionPool
+initDb config =
+  makeSnaplet "connection-pool" "A simple Postgresql connection pool" Nothing .
+  liftIO . runStdoutLoggingT $
+  createPostgresqlPool (view config connectionString)
+                       (view config poolSize)
+
+queryHandler :: SqlPersistM b -> Handler a ConnectionPool b
+queryHandler query =
+  do connection <- get
+     liftIO $
+       runSqlPersistMPool query connection
