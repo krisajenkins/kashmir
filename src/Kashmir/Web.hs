15e6be64970b294124f5fca417697b6c6cb6012f
Kris Jenkins <krisajenkins@gmail.com>
2015-08-17 14:30:02 +0100

    Adding a snap json-encoding utility.

--- src/Kashmir/Web.hs
+++ src/Kashmir/Web.hs
@@ -1,5 +1,6 @@
 module Kashmir.Web where
 
+import           Data.Aeson
 import           Data.ByteString
 import           Snap
 
@@ -8,3 +9,9 @@ mimeTypeJson = "application/json"
 
 jsonResponse :: MonadSnap m => m ()
 jsonResponse = modifyResponse $ setContentType mimeTypeJson
+
+writeJSON :: (ToJSON a,MonadSnap m)
+          => a -> m ()
+writeJSON a =
+  do jsonResponse
+     (writeLBS . encode) a
