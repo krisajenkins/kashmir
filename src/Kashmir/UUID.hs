36287d43c03c13af19478ea8641cea10afbf2271
Kris Jenkins <krisajenkins@gmail.com>
2016-01-18 07:47:53 +0000

    Code-formatting changes.

--- src/Kashmir/UUID.hs
+++ src/Kashmir/UUID.hs
@@ -18,12 +18,11 @@ import           Web.PathPieces
 instance PersistField UUID where
   toPersistValue :: UUID -> PersistValue
   toPersistValue = PersistDbSpecific . encodeUtf8 . toText
-  fromPersistValue :: PersistValue -> Either Text UUID
+  fromPersistValue
+    :: PersistValue -> Either Text UUID
   fromPersistValue (PersistDbSpecific s) =
     case (fromText . decodeUtf8) s of
-      Nothing ->
-        Left $
-        mconcat ["Could not parse UUID: ",decodeUtf8 s]
+      Nothing -> Left $ mconcat ["Could not parse UUID: ",decodeUtf8 s]
       Just uuid -> Right uuid
   fromPersistValue x = Left . pack $ "Not a PersistText type: " <> show x
 
@@ -31,9 +30,12 @@ instance PersistFieldSql UUID where
   sqlType _ = SqlOther "UUID"
 
 instance FromJSON UUID where
-  parseJSON = withText "UUID" (\t -> case fromText t of
-                                       Nothing -> fail (unpack t)
-                                       Just uuid -> return uuid)
+  parseJSON =
+    withText "UUID"
+             (\t ->
+                case fromText t of
+                  Nothing -> fail (unpack t)
+                  Just uuid -> return uuid)
 
 instance ToJSON UUID where
   toJSON = String . toText
