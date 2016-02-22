9b4cffcfad13aa6766384359f1cef3a1d3cefd08
Kris Jenkins <krisajenkins@gmail.com>
2016-02-22 13:37:16 +0000

    Removing some duplication in Utils.

--- src/Kashmir/Snap/Utils.hs
+++ src/Kashmir/Snap/Utils.hs
@@ -9,31 +9,23 @@ import           Database.Persist   ()
 import           Prelude
 import           Snap
 
-requireParam :: MonadSnap m
-             => ByteString -> m ByteString
-requireParam name =
-  do value <- getParam name
+mandatory
+  :: MonadSnap m
+  => (ByteString -> m (Maybe b)) -> ByteString -> m b
+mandatory lookupFn name =
+  do value <- lookupFn name
      case value of
        Just x -> return x
        Nothing ->
          do modifyResponse $
-              setResponseStatus
-                400
-                (encodeUtf8 "Missing parameter: " <>
-                 name)
+              setResponseStatus 400
+                                (encodeUtf8 "Missing parameter: " <> name)
             response <- getResponse
             finishWith response
 
+requireParam :: MonadSnap m
+             => ByteString -> m ByteString
+requireParam = mandatory getParam
+
 requirePostParam :: MonadSnap m => ByteString -> m ByteString
-requirePostParam name =
-  do value <- getPostParam name
-     case value of
-       Just x -> return x
-       Nothing ->
-         do modifyResponse $
-              setResponseStatus
-                400
-                (encodeUtf8 "Missing parameter: " <>
-                 name)
-            response <- getResponse
-            finishWith response
+requirePostParam = mandatory getPostParam
