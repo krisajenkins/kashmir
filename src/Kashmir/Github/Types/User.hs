00b4dcddfc87d910c943a150627f046ae49846cf
Kris Jenkins <krisajenkins@gmail.com>
2016-02-23 11:28:46 +0000

    Expanding the github User type to include all fields.

--- src/Kashmir/Github/Types/User.hs
+++ src/Kashmir/Github/Types/User.hs
@@ -7,26 +7,42 @@ import           Control.Lens
 import           Data.Aeson.Casing
 import           Data.Aeson.TH               (deriveJSON)
 import           Data.Text
+import           Data.Time
 import           GHC.Generics
+import           Kashmir.Email
 import           Kashmir.Github.Types.Common
 
 -- https://developer.github.com/v3/users/
 data User =
-  User {_id                :: Int
-       ,_login             :: Text
-       ,_name              :: Text
-       ,_email             :: Maybe Text
+  User {_login             :: Text
+       ,_id                :: Int
        ,_avatarUrl         :: URL
+       ,_gravatarId        :: Maybe URL
+       ,_url               :: URL
        ,_htmlUrl           :: URL
+       ,_followersUrl      :: URL
+       ,_followingUrl      :: URL
+       ,_gistsUrl          :: URL
+       ,_starredUrl        :: URL
+       ,_subscriptionsUrl  :: URL
+       ,_organizationsUrl  :: URL
        ,_reposUrl          :: URL
+       ,_eventsUrl         :: URL
        ,_receivedEventsUrl :: URL
+       ,_siteAdmin         :: Bool
+       ,_name              :: Maybe Text
        ,_company           :: Maybe Text
        ,_blog              :: Maybe Text
-       ,_location          :: Text
+       ,_location          :: Maybe Text
+       ,_email             :: Email
        ,_hireable          :: Bool
        ,_bio               :: Maybe Text
-       ,_followers         :: Integer
-       ,_following         :: Integer}
+       ,_publicRepos       :: Int
+       ,_publicGists       :: Int
+       ,_followers         :: Int
+       ,_following         :: Int
+       ,_createdAt         :: UTCTime
+       ,_updatedAt         :: UTCTime}
   deriving (Show,Eq,Generic)
 
 makeLenses ''User
