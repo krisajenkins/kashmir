03667f91d477b597ba27c82667c74c9ec9f8842a
Kris Jenkins <krisajenkins@gmail.com>
2016-03-03 10:48:50 +0000

    Making unfoldrM more generic.
    It now handles any monoid, not just a list.

--- src/Kashmir/ETL/Unfold.hs
+++ src/Kashmir/ETL/Unfold.hs
@@ -1,14 +1,15 @@
 module Kashmir.ETL.Unfold (unfoldrM) where
 
+import           Data.Monoid
 -- TODO Useful, but doesn't belong in this project.
 
 -- | Monadic Unfold.
-unfoldrM :: Monad m
-         => (b -> m (Maybe (a,b))) -> b -> m [a]
+unfoldrM :: (Monad m,Monoid r)
+         => (b -> m (Maybe (r,b))) -> b -> m r
 unfoldrM f b =
   do r <- f b
      case r of
        Just (a,b') ->
          do as <- unfoldrM f b'
-            return $ a : as
-       Nothing -> return []
+            return $ a <> as
+       Nothing -> return mempty
