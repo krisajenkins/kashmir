f38af0e01fdec7f4ec6f3d6909335bb74d27c707
Kris Jenkins <krisajenkins@gmail.com>
2016-11-01 15:00:00 +0000

    Whitespace changes.

--- src/Kashmir/Email.hs
+++ src/Kashmir/Email.hs
@@ -1,7 +1,12 @@
 {-# LANGUAGE DeriveAnyClass    #-}
 {-# LANGUAGE DeriveGeneric     #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Kashmir.Email (Email(), unEmail, parseEmail) where
+
+module Kashmir.Email
+  ( Email()
+  , unEmail
+  , parseEmail
+  ) where
 
 import           Data.Aeson
 import           Data.Text
@@ -9,22 +14,22 @@ import           Database.Persist
 import           Database.Persist.Sql
 import           GHC.Generics
 
-newtype Email =
-  Email {unEmail :: Text}
-  deriving (Show,Read,Eq,Generic)
+newtype Email = Email
+    { unEmail :: Text
+    } deriving (Show, Read, Eq, Generic)
 
 parseEmail :: Text -> Either Text Email
 parseEmail t = Right (Email t)
 
 instance PersistField Email where
-  toPersistValue (Email t) = toPersistValue t
-  fromPersistValue t = Email <$> fromPersistValue t
+    toPersistValue (Email t) = toPersistValue t
+    fromPersistValue t = Email <$> fromPersistValue t
 
 instance PersistFieldSql Email where
-  sqlType _ = SqlOther "text"
+    sqlType _ = SqlOther "text"
 
 instance ToJSON Email where
-  toJSON (Email t) = toJSON t
+    toJSON (Email t) = toJSON t
 
 instance FromJSON Email where
-  parseJSON t = Email <$> parseJSON t
+    parseJSON t = Email <$> parseJSON t
